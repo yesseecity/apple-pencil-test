24e3e4e0b911fc9e8bd284c6e998da6332b9033d
Tid Huang <yesseecity@yahoo.com.tw>
2024-12-27 17:32:31 +0800

    Log Apple Pencil Pro events

--- pencil.obj-c/CustomView.m
+++ pencil.obj-c/CustomView.m
@@ -13,8 +13,60 @@
 - (void)handleMouseEvent:(NSEvent *)event {
     if (event.type == NSEventTypePressure) {
         NSLog(@"Pressure: %f", event.pressure);
+    } else if (event.type == NSEventTypeRotate) {
+        NSLog(@"Rotation: %f", event.rotation);
     } else {
+        NSLog(@"Unknown event type: %@", [self stringForEventType:event.type]);
+        
         NSLog(@"Pressure: %f", event.pressure);
+        NSLog(@"Rotation: %f", event.rotation);
+        NSLog(@"Tilt X: %f, Tilt Y: %f", event.tilt.x, event.tilt.y);
+    }
+}
+
+// Override changeModeWithEvent: method
+- (void)changeModeWithEvent:(NSEvent *)event {
+    // NSLog(@"Mode changed with event: %@", [self stringForEventType:event.type]);
+    NSLog(@"Mode changed with event: %@", event);
+
+}
+
+// Helper method to convert NSEventType to string
+- (NSString *)stringForEventType:(NSEventType)type {
+    switch (type) {
+        case NSEventTypeLeftMouseDown:
+            return @"LeftMouseDown";
+        case NSEventTypeLeftMouseUp:
+            return @"LeftMouseUp";
+        case NSEventTypeRightMouseDown:
+            return @"RightMouseDown";
+        case NSEventTypeRightMouseUp:
+            return @"RightMouseUp";
+        case NSEventTypeMouseMoved:
+            return @"MouseMoved";
+        case NSEventTypeLeftMouseDragged:
+            return @"LeftMouseDragged";
+        case NSEventTypeRightMouseDragged:
+            return @"RightMouseDragged";
+        case NSEventTypeMouseEntered:
+            return @"MouseEntered";
+        case NSEventTypeMouseExited:
+            return @"MouseExited";
+        case NSEventTypePressure:
+            return @"Pressure";
+        case NSEventTypeRotate:
+            return @"Rotate";
+        default:
+            return [NSString stringWithFormat:@"Unknown (%lu)", type];
+    }
+}
+
+// Example method to demonstrate secure coding
+- (void)decodeWithCoder:(NSCoder *)coder {
+    if ([coder allowsKeyedCoding]) {
+        // Specify the exact class you expect to decode
+        NSString *exampleString = [coder decodeObjectOfClass:[NSString class] forKey:@"exampleKey"];
+        NSLog(@"Decoded string: %@", exampleString);
     }
 }
 
